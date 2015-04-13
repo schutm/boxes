37e05cc94b5b0c0af1a454eacc64625bc47cc1cd
Martin Schut <github@wommm.nl>
2015-04-13 23:22:57 +0200

    Make scripts dependent on each other, noteable nodejs.sh

--- provisioners/nodejs.sh
+++ provisioners/nodejs.sh
@@ -3,12 +3,14 @@
 # curl is required to install npm
 sudo aptitude -y install --without-recommends curl libcurl3 librtmp0 libssh2-1
 
-# nodejs and related binaries are in wheezy-backports
-sudo sh -c "echo deb http://http.debian.net/debian wheezy-backports main > /etc/apt/sources.list.d/wheezy-backports.list"
-sudo apt-get update
+# nodejs and related binaries from nodesource (more up to date than wheezy-backports)
+curl -sL https://deb.nodesource.com/setup | bash -
 
 # install nodejs
-sudo aptitude -y -t wheezy-backports install --without-recommends nodejs nodejs-legacy libv8-3.14.5 libc-ares2
+apt-get install -y nodejs nodejs-legacy
+
+# install build tools
+apt-get install -y build-essential
 
 # install npm from the live sources
 npm -v > /dev/null || curl -L https://npmjs.org/install.sh | sudo sh
